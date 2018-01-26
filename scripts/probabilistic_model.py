8f77cea73318043b3b51d8150deab9a78598c119
elggem <trilly@gmail.com>
2018-01-26 17:58:00 +0800

    emopy

---
+++ scripts/probabilistic_model.py
@@ -0,0 +1,7 @@
+"""
+This process listens to events from the dlib face processing to build a probabilistic model of faces perceived.
+
+Optionally it uses /orb2_slam localization output to build a global map of probabilities.
+
+
+"""
