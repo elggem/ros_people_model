f70652bb8dd6434a75e147caf553e2e5e785f696
elggem <trilly@gmail.com>
2018-02-15 18:57:35 +0800

    preliminary world model code

--- scripts/model_world.py
+++ scripts/model_world.py
@@ -11,30 +11,63 @@ import rospy
 import sys
 import numpy as np
 import image_geometry
+import tf
 
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
 from sensor_msgs.msg import CameraInfo
+from ros_peoplemodel.msg import Features
 
 CAMERA_MODEL = image_geometry.PinholeCameraModel()
 CAMERA_FOCAL_X = 0.0
 CAMERA_FOCAL_Y = 0.0
 
+POSE_QUATERNION = [0.0,0.0,0.0,0.0]
+
+# rotate vector v1 by quaternion q1
+# https://answers.ros.org/question/196149/how-to-rotate-vector-by-quaternion-in-python/
+def qv_mult(q1, v1):
+    v1 = tf.transformations.unit_vector(v1)
+    q2 = list(v1)
+    q2.append(0.0)
+    return tf.transformations.quaternion_multiply(
+        tf.transformations.quaternion_multiply(q1, q2),
+        tf.transformations.quaternion_conjugate(q1)
+    )[:3]
+
+def projectPixelAndPoseTo3dRay(uv):
+    global POSE_QUATERNION
+    return qv_mult(POSE_QUATERNION, CAMERA_MODEL.projectPixelTo3dRay(uv))
+
+
+
+
+def poseCallback(pose):
+    global POSE_QUATERNION
+    POSE_QUATERNION = [pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z]
 
 def cameraInfoCallback(msg):
-    global CAMERA_MODEL, CAMERA_FOCAL_X, CAMERA_FOCAL_Y
+    global CAMERA_MODEL
     CAMERA_MODEL.fromCameraInfo(msg)
-    CAMERA_FOCAL_X = CAMERA_MODEL.fx()
-    CAMERA_FOCAL_Y = CAMERA_MODEL.fy()
+
+def featuresCallback(features):
+    for roi in features.rois:
+        uv = (roi.x_offset + (roi.width/2), roi.y_offset + (roi.height/2))
+        ray = projectPixelAndPoseTo3dRay(uv)
+        print ray
+    pass
+
 
 if __name__ == "__main__":
-    rospy.init_node('pau_pose_estimator', anonymous=True)
+    rospy.init_node('model_world', anonymous=True)
 
     # Publishers
-    pub = rospy.Publisher('/pose', Pose, queue_size=10)
+    # pub = rospy.Publisher('/pose', Pose, queue_size=10)
     # Subscribers
+    rospy.Subscriber("/pose", Pose, poseCallback)
     rospy.Subscriber("/camera/camera_info", CameraInfo, cameraInfoCallback)
 
-    # Launch drawing timer
-    rospy.Timer(rospy.Duration(1.0/10.0), outputValues)
+    # TODO: replace with people model
+    rospy.Subscriber("/people/vis_dlib_cnn", Features, featuresCallback)
 
     rospy.spin()
