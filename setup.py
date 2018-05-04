5dad51369ebf7035f84d715f6aff5bafd11714a5
Jamie Diprose <jamie.diprose@gmail.com>
2018-05-04 17:03:52 +0800

    Change ros_people_model package to recognisers package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 from distutils.core import setup
 
 d = generate_distutils_setup(
-    packages=['ros_people_model'],
+    packages=['recognisers'],
     package_dir={'': 'src'}
 )
 setup(**d)
