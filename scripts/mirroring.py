3719a1e0e79a2dfa6f17013f7d5041183f5a14c5
Jamie Diprose <jamie.diprose@gmail.com>
2018-05-31 17:59:39 +0800

    Add ability to sync mirroring fps

--- scripts/mirroring.py
+++ scripts/mirroring.py
@@ -2,62 +2,76 @@
 import rospy
 from blender_api_msgs.msg import EmotionState
 from ros_people_model.msg import Faces
-
-EMOTIONS = ['anger', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
-EXPRESSIONS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprised', 'none']
-WEIGHTS = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.0]
-THRESHOLD = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-STATES = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-DECAY = 0.08
+from threading import Lock
 
 
-class Tracking:
+class Mirroring:
+    PERSON_EMOTIONS = ['anger', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+    ROBOT_EMOTIONS = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprised', 'none']
+    WEIGHTS = [0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.0]
+    THRESHOLD = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+    DECAY = 0.08
+
     def __init__(self):
-        self.expressions_pub = rospy.Publisher('/blender_api/set_emotion_value', EmotionState, queue_size=1)
+        self.states_lock = Lock()
+        self.states = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        self.emotion_pub = rospy.Publisher('/blender_api/set_emotion_value', EmotionState, queue_size=1)
         self.biggest_face = None
 
-        rospy.Subscriber("/faces", Faces, self.faces_perceived)
-        rospy.Timer(rospy.Duration(1.0 / 25.0), self.update_head_position)
+        self.fps = rospy.get_param('~fps', 20)
+
+        rospy.Subscriber("/faces", Faces, self.faces_perceived_cb)
+        rospy.Timer(rospy.Duration(1.0 / self.fps), self.update_robot_emotions_cb)
 
     def get_emotion_value(self, emotype):
-        return self.biggest_face.emotions[EMOTIONS.index(emotype)]
+        return self.biggest_face.emotions[Mirroring.PERSON_EMOTIONS.index(emotype)]
 
-    def update_head_position(self, event):
-        rospy.logdebug(' '.join(['%s: %.3f' % (EMOTIONS[n], STATES[n]) for n in xrange(len(EMOTIONS))]))
+    def update_robot_emotions_cb(self, event):
+        with self.states_lock:
+            rospy.logdebug(' '.join(['%s: %.3f' % (Mirroring.PERSON_EMOTIONS[n], self.states[n]) for n in
+                                     xrange(len(Mirroring.PERSON_EMOTIONS))]))
 
-        for i, emo in enumerate(EMOTIONS):
-            expression = EmotionState()
-            expression.name = EXPRESSIONS[i]
-            expression.magnitude = STATES[i]
-            if expression.magnitude > 0.005:
-                self.expressions_pub.publish(expression)
+            for i, emo in enumerate(Mirroring.PERSON_EMOTIONS):
+                expression = EmotionState()
+                expression.name = Mirroring.ROBOT_EMOTIONS[i]
+                expression.magnitude = self.states[i]
+                if expression.magnitude > 0.005:
+                    self.emotion_pub.publish(expression)
 
-    def faces_perceived(self, faces):
-        if len(faces.faces) > 0:
-            self.biggest_face = faces.faces[0]
-            for face in faces.faces:
-                if self.biggest_face.position.z < face.position.z:
-                    self.biggest_face = face
+    def faces_perceived_cb(self, faces):
+        with self.states_lock:
+            if len(faces.faces) > 0:
+                self.biggest_face = faces.faces[0]
+                for face in faces.faces:
+                    if self.biggest_face.position.z < face.position.z:
+                        self.biggest_face = face
 
-            if len(self.biggest_face.emotions) > 1:
-                try:
-                    for i, emo in enumerate(EMOTIONS):
-                        if self.get_emotion_value(emo) > THRESHOLD[i]:
-                            STATES[i] = (STATES[i] * (1.0 - WEIGHTS[i])) + (WEIGHTS[i] * self.get_emotion_value(emo))
-                        else:
-                            STATES[i] = STATES[i] * (1.0 - DECAY)
-                except:
-                    pass
+                if len(self.biggest_face.emotions) > 1:
+                    try:
+                        for i, emo in enumerate(Mirroring.PERSON_EMOTIONS):
+                            if self.get_emotion_value(emo) > Mirroring.THRESHOLD[i]:
+                                self.states[i] = (self.states[i] * (1.0 - Mirroring.WEIGHTS[i])) + (
+                                            Mirroring.WEIGHTS[i] * self.get_emotion_value(emo))
+                            else:
+                                self.states[i] = self.states[i] * (1.0 - Mirroring.DECAY)
+                    except:
+                        pass
+                else:
+                    for i, emo in enumerate(Mirroring.PERSON_EMOTIONS):
+                        self.states[i] = self.states[i] * (1.0 - Mirroring.DECAY)
             else:
-                for i, emo in enumerate(EMOTIONS):
-                    STATES[i] = STATES[i] * (1.0 - DECAY)
-        else:
-            self.biggest_face = None
-            for i, emo in enumerate(EMOTIONS):
-                STATES[i] = STATES[i] * (1.0 - DECAY)
+                self.biggest_face = None
+                for i, emo in enumerate(Mirroring.PERSON_EMOTIONS):
+                    self.states[i] = self.states[i] * (1.0 - Mirroring.DECAY)
 
 
-rospy.loginfo("init")
-rospy.init_node('FaceTracking')
-node = Tracking()
-rospy.spin()
+if __name__ == "__main__":
+
+    try:
+        rospy.loginfo("init")
+        rospy.init_node('Mirroring')
+        node = Mirroring()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
